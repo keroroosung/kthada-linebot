2399b94baf087c9a0d361836c7fbc49b788d16b9
Thada Kullapha <k.thada@kkumail.com>
2020-02-27 19:31:13 +0700

    Add file it-yth-bot.php

--- it-yth-bot.php
+++ it-yth-bot.php
@@ -11,6 +11,8 @@ error_reporting(E_ALL);
  
 // include composer autoload
 require_once 'vendor/autoload.php';
+require_once 'model-bot.php';
+$modelDutyIT = new modelDutyIT();
  
 // การตั้งเกี่ยวกับ bot
  
@@ -71,74 +73,11 @@ if(!is_null($events)){
     switch ($typeMessage){
         case 'text':
             switch ($userMessage) {
-                case "t":
-                    $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
+                case "เวรไอที":
+                    $textReplyMessage = $modelDutyIT->getITDuty();
                     $replyData = new TextMessageBuilder($textReplyMessage);
-                    break;
-                case "i":
-                    $picFullSize = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower';
-                    $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower/240';
-                    $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
-                    break;
-                case "v":
-                    $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/240';
-                    $videoUrl = "https://www.mywebsite.com/simplevideo.mp4";                
-                    $replyData = new VideoMessageBuilder($videoUrl,$picThumbnail);
-                    break;
-                case "a":
-                    $audioUrl = "https://www.mywebsite.com/simpleaudio.mp3";
-                    $replyData = new AudioMessageBuilder($audioUrl,27000);
-                    break;
-                case "l":
-                    $placeName = "ที่ตั้งร้าน";
-                    $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
-                    $latitude = 13.780401863217657;
-                    $longitude = 100.61141967773438;
-                    $replyData = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);              
-                    break;
-                case "s":
-                    $stickerID = 22;
-                    $packageID = 2;
-                    $replyData = new StickerMessageBuilder($packageID,$stickerID);
-                    break;      
-                case "im":
-                    $imageMapUrl = 'https://www.mywebsite.com/imgsrc/photos/w/sampleimagemap';
-                    $replyData = new ImagemapMessageBuilder(
-                        $imageMapUrl,
-                        'This is Title',
-                        new BaseSizeBuilder(699,1040),
-                        array(
-                            new ImagemapMessageActionBuilder(
-                                'test image map',
-                                new AreaBuilder(0,0,520,699)
-                                ),
-                            new ImagemapUriActionBuilder(
-                                'http://www.ninenik.com',
-                                new AreaBuilder(520,0,520,699)
-                                )
-                        )); 
-                    break;          
-                case "tm":
-                    $replyData = new TemplateMessageBuilder('Confirm Template',
-                        new ConfirmTemplateBuilder(
-                                'Confirm template builder',
-                                array(
-                                    new MessageTemplateActionBuilder(
-                                        'Yes',
-                                        'Text Yes'
-                                    ),
-                                    new MessageTemplateActionBuilder(
-                                        'No',
-                                        'Text NO'
-                                    )
-                                )
-                        )
-                    );
-                    break;                                                                                                                          
-                default:
-                    $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
-                    $replyData = new TextMessageBuilder($textReplyMessage);         
-                    break;                                      
+                    $response = $bot->replyMessage($replyToken,$replyData);
+                    break;                                     
             }
             break;
         default:
@@ -148,6 +87,80 @@ if(!is_null($events)){
     }
 }
 //l ส่วนของคำสั่งตอบกลับข้อความ
-$response = $bot->replyMessage($replyToken,$replyData);
 
+
+/*
+switch ($userMessage) {
+  case "t":
+      $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
+      $replyData = new TextMessageBuilder($textReplyMessage);
+      $response = $bot->replyMessage($replyToken,$replyData);
+      break;
+  case "i":
+      $picFullSize = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower';
+      $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower/240';
+      $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
+      $response = $bot->replyMessage($replyToken,$replyData);
+      break;
+  case "v":
+      $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/240';
+      $videoUrl = "https://www.mywebsite.com/simplevideo.mp4";                
+      $replyData = new VideoMessageBuilder($videoUrl,$picThumbnail);
+      $response = $bot->replyMessage($replyToken,$replyData);
+      break;
+  case "a":
+      $audioUrl = "https://www.mywebsite.com/simpleaudio.mp3";
+      $replyData = new AudioMessageBuilder($audioUrl,27000);
+      break;
+  case "l":
+      $placeName = "ที่ตั้งร้าน";
+      $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
+      $latitude = 13.780401863217657;
+      $longitude = 100.61141967773438;
+      $replyData = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);              
+      break;
+  case "s":
+      $stickerID = 22;
+      $packageID = 2;
+      $replyData = new StickerMessageBuilder($packageID,$stickerID);
+      break;      
+  case "im":
+      $imageMapUrl = 'https://www.mywebsite.com/imgsrc/photos/w/sampleimagemap';
+      $replyData = new ImagemapMessageBuilder(
+          $imageMapUrl,
+          'This is Title',
+          new BaseSizeBuilder(699,1040),
+          array(
+              new ImagemapMessageActionBuilder(
+                  'test image map',
+                  new AreaBuilder(0,0,520,699)
+                  ),
+              new ImagemapUriActionBuilder(
+                  'http://www.ninenik.com',
+                  new AreaBuilder(520,0,520,699)
+                  )
+          )); 
+      break;          
+  case "tm":
+      $replyData = new TemplateMessageBuilder('Confirm Template',
+          new ConfirmTemplateBuilder(
+                  'Confirm template builder',
+                  array(
+                      new MessageTemplateActionBuilder(
+                          'Yes',
+                          'Text Yes'
+                      ),
+                      new MessageTemplateActionBuilder(
+                          'No',
+                          'Text NO'
+                      )
+                  )
+          )
+      );
+      break;                                                                                                                          
+  default:
+      $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
+      $replyData = new TextMessageBuilder($textReplyMessage);         
+      break;                                      
+}*/
 ?>
